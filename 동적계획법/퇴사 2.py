1b243f45e69338706dad87e7d3df9975372da603
kmin6 <kmin638123@gmail.com>
2024-04-02 23:58:41 +0900

    dp_백준

--- 동적계획법/퇴사 2.py
+++ 동적계획법/퇴사 2.py
@@ -8,7 +8,7 @@ s = [list(map(int, input().split())) for _ in range(n)]
 dp = [0] * (n+1)
 
 for i in range(n):
-    dp[i+1] = max(dp[i+1],dp[i])
+    dp[i+1] = max(dp[i+1],dp[i]) # 이전까지 수익과, 현재의 수익 비교
     if i+s[i][0]<=n:
         dp[i+s[i][0]]=max(dp[i+s[i][0]], dp[i]+s[i][1])
         
